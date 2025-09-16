c4b9f72daeca08134b82975b3edbfb51bfd64c6d
Luis Osar Gonzalez <gonzalezluisoscar393@gmail.com>
2025-09-15 21:06:00 -0300

    Primer commit

---
+++ Biblioteca.Services/StringServices.cs
@@ -0,0 +1,14 @@
+﻿namespace Biblioteca.Services
+{
+    public interface IStringServices
+    {
+        string GetString(string stn);
+    }
+    public class StringServices : IStringServices
+    {
+        public string GetString(string stn)
+        {
+            return string.Join(" ", stn, "Funciona");
+        }
+    }
+}
---
+++ Biblioteca.WebApi/Models/Libro.cs
@@ -0,0 +1,9 @@
+﻿namespace Biblioteca.WebApi.Models
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Pages { get; set; }
+    }
+}
