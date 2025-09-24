54145066d294f432c246913f0b54568e348994ed
Luis Osar Gonzalez <gonzalezluisoscar393@gmail.com>
2025-09-23 21:10:23 -0300

    Clase 23/9

--- Biblioteca.WebApi/Models/Libro.cs
+++ Biblioteca.WebApi/Models/Libro.cs
@@ -1,8 +1,11 @@
-﻿namespace Biblioteca.WebApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca.WebApi.Models
 {
     public class Libro
     {
         public int Id { get; set; }
+        [StringLength(10)]
         public string Nombre { get; set; }
         public int Pages { get; set; }
     }
